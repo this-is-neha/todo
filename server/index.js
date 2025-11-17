7f83470809f0fb1569d825ad5b3b015504637862
this-is-neha <nehushah12@gmail.com>
2025-11-17 09:28:21 +0545

    Neha

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const http = require('http');
-
+const PORT = process.env.PORT || 3001;
 const app = require("./src/config/express.config")
 const server=http.createServer(app);    
-server.listen(3001,(err)=>{
+server.listen(PORT,(err)=>{
     if(!err){
         console.log("Server is running on port 3001")
         console.log("Press Ctrl + C to stop the server")
